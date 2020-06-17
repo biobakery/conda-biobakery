e67acec4b9c3aeddaac3c23c66c7242f8d460714
ljmciver <lauren.j.mciver@gmail.com>
2020-06-17 15:44:34 -0400

    Update workflows to version 3

--- recipes/biobakery_workflows/build.sh
+++
@@ -1,5 +0,0 @@
-
-python setup.py install --single-version-externally-managed --record=record.txt
-
-# copy the database files since these are not installed with this method (as they are with pip from data_files)
-cp -r examples/tutorial $PREFIX/lib/python2.7/site-packages/
